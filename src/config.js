784b7a3a9b6ebcec683ba23fe9d79d57e7b77cbd
N1CHO <129211977+notN1CHO@users.noreply.github.com>
2023-03-28 18:18:21 +0000

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,22 @@
+/*
+
+/////////#/#/#/#/#/#/////////
+
+Made By: N1CHO
+
+Discord User: N1CHO#6559
+
+Instagram: samuske1
+
+/////////#/#/#/#/#/#/////////
+
+*/
+
+
+module.exports = {
+  prefix: "/",
+  owner: "704896961563590767", //Bot Owner ID
+  serverip: "164.92.175.207", //Hosting Server Ip
+  ip: "https://panel.ares-hosting.xyz", //API Server
+  token: "Bot's token" //Your bot token
+}
