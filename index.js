020a239fda0bfc89c0ad231fc7a801075739c6d3
N1CHO <129211977+notN1CHO@users.noreply.github.com>
2023-03-28 18:03:05 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const config = require("./src/config.js");
+const { readdirSync } = require("fs")
+const moment = require("moment");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+let token = config.token
+
+client.commands = new Collection()
+
+const rest = new REST({ version: '10' }).setToken(process.env.token);
+
+const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
+
+//command-handler
+const commands = [];
+readdirSync('./src/commands').forEach(async file => {
+  const command = require(`./src/commands/${file}`);
+  commands.push(command.data.toJSON());
+  client.commands.set(command.data.name, command);
+})
+
+client.on("ready", async () => {
+        try {
+            await rest.put(
+                Routes.applicationCommands(client.user.id),
+                { body: commands },
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    log(`${client.user.username} is Online!`);
+})
+
+//event-handler
+readdirSync('./src/events').forEach(async file => {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+})
+
+//nodejs-events
+process.on("unhandledRejection", e => { 
+   console.log(e)
+ }) 
+process.on("uncaughtException", e => { 
+   console.log(e)
+ })  
+process.on("uncaughtExceptionMonitor", e => { 
+   console.log(e)
+ })
+//
+setTimeout(() => {
+  if (!client || !client.user) {
+    console.log("Client Not Login, Process Kill")
+    process.kill(1);
+  } else {
+    console.log("Client Login")
+  }
+}, 9*1000);
+
+client.login(process.env.token)
