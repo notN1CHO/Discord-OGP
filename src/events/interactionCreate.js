85e2177f2a65941960b21883b877fa27a0e89aa4
N1CHO <129211977+notN1CHO@users.noreply.github.com>
2023-03-28 18:09:36 +0000

    Add files via upload

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,16 @@
+const { InteractionType } = require("discord.js");
+
+ module.exports = {
+	name: 'interactionCreate',
+	execute: async(interaction) => {
+         let client = interaction.client;
+   	 if (interaction.type == InteractionType.ApplicationCommand) {
+   	 if(interaction.user.bot) return;
+	try {
+         const command = client.commands.get(interaction.commandName)
+         command.run(client, interaction)
+	} catch {
+	interaction.reply({content: "Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.", ephemeral: true})
+	}
+	 }
+  }}
