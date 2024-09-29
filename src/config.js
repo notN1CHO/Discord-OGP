e910b42b79d69ac01d35acb438c9e0f58cf074f9
N1CHO <129211977+notN1CHO@users.noreply.github.com>
2024-09-29 23:14:30 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,22 +1,29 @@
 /*
 
-/////////#/#/#/#/#/#/////////
+//////////////////#/#/#/#/#/#//////////////////
 
-Made By: N1CHO
+                Made By: N1CHO
 
-Discord User: N1CHO#6559
+        Discord User: n1cho. (N1CHO#6559)
 
-Instagram: samuske1
+              Instagram: n_1cho
 
-/////////#/#/#/#/#/#/////////
+//////////////////#/#/#/#/#/#//////////////////
 
 */
 
 
 module.exports = {
+  GuildID: "", //Your Discord server ID for channel logs
+  ChannelID: "", //Your text channel ID for creating server logs
   prefix: "/",
-  owner: "704896961563590767", //Bot Owner ID
-  serverip: "164.92.175.207", //Hosting Server Ip
-  ip: "https://panel.ares-hosting.xyz", //API Server
-  token: "Bot's token" //Your bot token
+  owner: "704896961563590767",//Owner ID
+  serverip: "210.79.134.166", //Server IP for hosting
+  ip: "http://210.79.134.166",//Your panel ip (Ex: panel.ares-host.com)
+  remote_server_id: "1", //Your remote server ID (change this based on your remote ID)
+  token: "",//Bot Token
+  mysqlhost: "210.79.134.166",//"5.252.100.174",
+  mysqluser: "root",//Mysql User
+  mysqlpw: "",//Mysql Password
+  mysqldb: "ogp_panel"//Mysql Database of OGP (Default: ogp_panel) 
 }
