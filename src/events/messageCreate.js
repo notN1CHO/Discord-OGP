14207629dd2324faadb2ea3fbfc8908af3903140
N1CHO <129211977+notN1CHO@users.noreply.github.com>
2024-09-29 23:12:12 +0100

    Delete src/events directory

--- src/events/messageCreate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'messageCreate',
-	execute: async(message) => {
-  let client = message.client;
-  if (message.author.bot) return;
-  if (message.channel.type === 'dm') return;
-  }};
--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-const { ActivityType } = require("discord.js")
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-  client.user.setActivity({ name: `Made By N1CHO#6559`, type: ActivityType.Listening });
-}};
